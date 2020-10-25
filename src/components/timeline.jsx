133c4ec078e679aae7fe1074073b96d1e1d145ee
mzhou08 <mzhou08@gmail.com>
2020-10-25 00:26:22 -0400

    modified timeline page

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -17,34 +17,67 @@ export default class Timeline extends Component {
                 <div className="timeline-centered">
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-3">
+                      <div className="timeline-icon color-1">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                        <h2><a target="_blank" href="https://fisher.wharton.upenn.edu/management-technology-summer-institute/">UPenn Wharton M&TSI</a> <span>July 2020</span></h2>
+                        <p>I spend the month with the M&TSI summer camp.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article>
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2><a target="_blank" href="https://www.acs.org/content/acs/en/education/students/highschool/olympiad.html">US National Chemistry Olympiad</a><span>May 2020</span></h2>
+                        <p>I am in <a target="_blank" href="file:///Users/zqian/Downloads/2020_usnco_high_honors_and_honors_list%20(3).pdf">2020 USNCO Honors list. </a> </p>
                       </div>
                     </div>
                   </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-4">
+                      <div className="timeline-icon color-2">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2> <a target="_blank" href = "http://centerbeijing.yale.edu/event/2019/07/yygs-beijing-asia-21st-century#:~:text=The%20program%20will%20take%20place,YYGS%2DBeijing%20staff%20about%20the">Yale Young Global Scholar</a><span>July 28 - August 10, 2019</span></h2>
+                        <p>I attended YYGS-BEIJING: ASIA in the 21st Century</p>
                       </div>
                     </div>
                   </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-5">
+                      <div className="timeline-icon color-3">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2> <a target="_blank" href="https://www.fte.org/students/economics-for-leaders-program/"> Economics for Leaders, University of California, Berkeley </a><span>July 1- July 6 2019</span></h2>
+                        <p>I attended the Economics for Leaders at UCB. </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2><a target="_blank" href="https://www.bsu.edu/calendar/events/academics/school-of-music/2019/06/24/mfa-summer-symposium">Music for All Summer Symposium</a> <span>June 25 - June 29 2019</span></h2>
+                        <p>A week-long music summer camp in Ball State University, IN. I am in the Marching Band and Drum Majors sessions.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> <a target="_blank" href="https://oakland.edu/math/outreach/ousmi/"> Summer Mathematics Institute, Oakland University </a><span>July 2018</span></h2>
+                        <p>I have completed two courses: linear algebra and  </p> 
                       </div>
                     </div>
                   </article>
