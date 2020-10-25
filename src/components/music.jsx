6b61089f7316bdfeb841103210f98dd9f49d6226
mzhou08 <mzhou08@gmail.com>
2020-10-24 23:01:10 -0400

    update jpeg file

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -28,7 +28,7 @@ export default class Music extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/marching_band.jpg)'}}>
+								<div className="project" style={{backgroundImage: 'url(images/marching_band.jpeg)'}}>
 									<div className="desc">
 										<div className="con">
 											<h3><a href="band.html">Marching Band</a></h3>
