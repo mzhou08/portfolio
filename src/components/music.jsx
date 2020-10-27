af8ed05d7b11ef2ce34d4c129df60fc3c59839e2
mzhou08 <mzhou08@gmail.com>
2020-10-27 01:05:37 -0400

    update to the music page

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -4,42 +4,52 @@ export default class Music extends Component {
   render() {
     return (
       <div>
-				<section className="colorlib-work" data-section="music">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Music</span>
-								<h2 className="colorlib-heading animate-box">Recent Music Performance</h2>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3>
-												<a href="https://www.saxophonealliance.org/conference-competitions.asp">
-													2020 NASA Competitions
-												</a>
-											</h3>
-											<span>Link to NASA submission</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/marching_band.jpeg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="band.html">Marching Band</a></h3>
-											<span>During my junior and senior years, I am the drum major of Skyline High School Marching Band. </span>
-										</div>
-									</div>
-								</div>
-							</div>
+			<section className="colorlib-work" data-section="music">
+				<div className="colorlib-narrow-content">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span className="heading-meta">My Music</span>
+							<h2 className="colorlib-heading animate-box">Recent Music Performance</h2>
 						</div>
 					</div>
-				</section>
+					<div className="row">
+						<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+							<iframe title='music1'
+								width="560" 
+								height="315" 
+								src="https://www.youtube.com/embed/N27Xowpde5g" 
+								frameborder="0" 
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+								allowfullscreen>
+							</iframe>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+							<iframe title='music2'
+								width="560" 
+								height="315" 
+								src="https://www.youtube.com/embed/w0F7J8L5kog" 
+								frameborder="0" 
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+								allowfullscreen>
+							</iframe>
+					</div>
+					<div className="row">
+						</div>
+						<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+							<iframe title='halftime'
+								width="560" 
+								height="315" 
+								src="https://www.youtube.com/embed/XI27pWVG-qk" 
+								frameborder="0" 
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+								allowfullscreen>
+							</iframe>
+						</div>
+					</div>
+				</div>
+			</section>
       </div>
     )
   }
