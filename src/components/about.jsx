a441b0385cba75efc465a0cd6b0e8bba907e207f
Michael Zhou <mzhou08@gmail.com>
2020-10-28 21:31:06 -0400

    updated drawings and descriptions. More work needed to reorder.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,7 +13,9 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a senior student in Skyline High School, Ann Arbor</p>
+                    <p>Hi, welcome to my site! I’m Michael Zhou, a high school senior at Ann Arbor Skyline High School in Michigan. 
+                        Here, I’ve documented some of the things I’ve done, from bioinformatics research to art to music. I hope this 
+                        site will help you learn a little bit more about me!</p>
                     </div>
                 </div>
                 </div>
@@ -25,8 +27,8 @@ export default class About extends Component {
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <span className="heading-meta">What do I do?</span>
+                <h2 className="colorlib-heading">Here are some of my skills:</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -37,7 +39,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Math</h3>
-                    <p>I spent years in Math competetion</p>
+                    <p>I've spent many years in math, not only competing but also teaching and creating new learning opportunities.</p>
                 </div>
                 </div>
             </div>
@@ -48,7 +50,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Programming</h3>
-                    <p>Here is my GitHub account</p>
+                    <p>Here is my GitHub account:</p>
                 </div>
                 </div>
             </div>
--- src/components/arts.jsx
+++ src/components/arts.jsx
@@ -8,8 +8,8 @@ export default class Arts extends Component {
 					<div className="colorlib-narrow-content">
 						<div className="row">
 							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Art Works</span>
-								<h2 className="colorlib-heading animate-box">Recent Arts</h2>
+								<span className="heading-meta">My Artwork</span>
+								<h2 className="colorlib-heading animate-box">Recent Artwork: Mouse Over for a Description!</h2>
 							</div>
 						</div>
 						<div className="row">
@@ -17,7 +17,7 @@ export default class Arts extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/hug.jpeg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Put some description here</a></h3>
+											<h3><a href="work.html">Hugs: Graphite</a></h3>
 											<span>Animation</span>
 										</div>
 									</div>
@@ -27,8 +27,12 @@ export default class Arts extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/art_car.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a target="new" href="cars.html">Toyota Dream Car - U.S. Gold Winner and World Best Finalist</a></h3>
-											<span>Here is the <a target = "new"  href="https://tfsinthecommunity.com/michael-zhou/">press coverage link</a>. For the 9th Toyota Dream Car Art Contest 2015, more than 875,000 entries were submitted from over 81 countries around the world. From those submissions, the top 700-plus artworks were forwarded to Japan for the World Contest judging. In the U.S. contest, Michael had earned the Gold Award for youth, ages 8-11 and was ultimately selected as one of the World Winners, later also winning Best Finalist for my age category globally.</span>
+											<h3>Handicap-Friendly Cleaning Car: Watercolor</h3>
+											<span>Here is the <a target = "new"  href="https://tfsinthecommunity.com/michael-zhou/">press coverage link</a>. 
+											For the 9th Toyota Dream Car Art Contest 2015, more than 875,000 entries were submitted from over 81 countries around the world. 
+											From those submissions, the top 700-plus artworks were forwarded to Japan for the World Contest judging. 
+											In the U.S. contest, I earned the Gold Award for youth, ages 8-11 and was ultimately selected as one of the World Winners, 
+											later also winning Best Finalist for my age category globally.</span>
 										</div>
 									</div>
 								</div>
@@ -37,7 +41,21 @@ export default class Arts extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/lion.jpeg)'}}>
 									<div className="desc">
 										<div className="con">
+											<h3>Lion: Oil Pastel</h3>
 											<span>A lion portrait.</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/bird_drawing.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3>Hyacinth Macaw: Color Pencil and Graphite</h3>
+											<span>This drawing of a hyacinth macaw was done with colored pencil and graphite. 
+											Colored pencil was used for the body, while the wings were drawn and detailed with graphite pencil.</span>
 										</div>
 									</div>
 								</div>
