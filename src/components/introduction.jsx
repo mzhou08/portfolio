d8fb99bf7f4c9934c89ab4a79eb25a227b26319d
mzhou08 <mzhou08@gmail.com>
2020-11-13 23:22:26 -0500

    updated background image

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,14 +7,15 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)', backgroundSize:'contain'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
-                  <div className="row">
+                  <div className="row" >
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
+                      <div className="slider-text-inner">
                         <div className="desc">
-                          <h1 style={{color:`white`}}> Hi! <br />I'm Michael Zhou.</h1>
+                          <h1 style={{color:`black`}}> Hi! I'm Michael Zhou.</h1>
                         </div>
                       </div>
                     </div>
